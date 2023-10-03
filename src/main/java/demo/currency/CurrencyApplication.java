1628b62750bcfa8ce1f8085897c15bce5e485f6d
byc <byckg3@gmail.com>
2023-10-03 19:24:41 +0800

    add validation for AppPropweties

--- src/main/java/demo/currency/CurrencyApplication.java
+++ src/main/java/demo/currency/CurrencyApplication.java
@@ -2,8 +2,10 @@ package demo.currency;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class CurrencyApplication {
 
 	public static void main( String[] args) {
--- src/main/java/demo/currency/config/AppProperties.java
+++ src/main/java/demo/currency/config/AppProperties.java
@@ -1,14 +1,18 @@
 package demo.currency.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 @Data
-@Component
+@Validated
 @ConfigurationProperties( prefix = "app.props" )
 public class AppProperties
 {
+    @NotNull( message = "page size must be not null" )
+    @Positive( message = "page size must be positive integer" )
     private Integer pageSize;
 }
